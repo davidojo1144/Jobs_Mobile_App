d0fd7526fd439c1b1bf82cbf3c216c1fa99a2db7
davidojo1144 <davco1144@gmail.com>
2025-06-20 04:48:34 +0100

    Mobile Application built for jobs using React Native

--- React_native_jobs/app/_layout.js
+++ React_native_jobs/app/_layout.js
@@ -6,6 +6,16 @@ import * as SplashScreen from "expo-splash-screen"
 SplashScreen.preventAutoHideAsync()
 
 const Layout = ()=> {
+    
+    const [fontsLoaded] = useFonts({
+        DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+        DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+        DMRegular: require("../assets/fonts/DMSans-Regular.ttf")
+    })
+
+    const onLayoutRootView = useCallback()
+
+
     return <Stack/>
 }
 
