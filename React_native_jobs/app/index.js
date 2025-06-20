17cfa251f671d367c0217066d50685800ad51e2e
davidojo1144 <davco1144@gmail.com>
2025-06-20 04:19:25 +0100

    Mobile Application built for jobs using React Native

--- React_native_jobs/app/index.js
+++ React_native_jobs/app/index.js
@@ -20,14 +20,27 @@ const Home = ()=> {
                 options={{
                     headerStyle: {backgroundColor: COLORS.lightWhite},
                     headerShadowVisible: false,
-                    headerLeft: () => {
+                    headerLeft: () => (
                         <ScreenHeaderBtn iconUrl={icons.menu} dimension = "60%"/>
-                    },
-                      headerRight: () => {
-                        <ScreenHeaderBtn iconUrl={icons.profile} dimension = "100%"/>
-                    }
+                    ),
+                      headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension = "100%"/>
+                      ),
+                      headerTitle: "",
                 }}
             />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                style={{
+                    flex: 1,
+                    padding: SIZES.medium
+                }}
+                >
+                    <Welcome/>
+                    <Popularjobs/>
+                    <Nearbyjobs/>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
