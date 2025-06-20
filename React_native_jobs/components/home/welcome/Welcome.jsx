f69625c5594478a6f32840c9323304fbd1b88953
davidojo1144 <davco1144@gmail.com>
2025-06-20 04:37:22 +0100

    Mobile Application built for jobs using React Native

--- React_native_jobs/components/home/welcome/Welcome.jsx
+++ React_native_jobs/components/home/welcome/Welcome.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { 
+  View, 
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList 
+} from 'react-native'
+import { useRouter } from 'expo-router'
 
 import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
 
 const Welcome = () => {
+  const router = useRouter()
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Dan</Text>
+        <Text style={styles.userName}>Ready to find your perfect job</Text>
+      </View>
     </View>
   )
 }
