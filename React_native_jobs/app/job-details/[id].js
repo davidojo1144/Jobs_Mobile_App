2188ce77411be0027b4fb9878722fcbfb36fa42d
davidojo1144 <davco1144@gmail.com>
2025-06-21 05:26:53 +0100

    Mobile Application built for jobs using React Native

--- React_native_jobs/app/job-details/[id].js
+++ React_native_jobs/app/job-details/[id].js
@@ -1,10 +1,32 @@
-import React from 'react'
+import {useState, useCallback} from 'react'
+import { 
+    View, 
+    Text,
+    SafeAreaView,
+    ScrollView,
+    ActivityIndicator,
+    RefreshControl
+} from 'react-native'
+import { Stack, useRouter, useSearchParams } from 'expo-router'
+
+
+import {Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from "../../components"
+import { COLORS, icons, SIZES } from '../../constants'
+import useFetch from '../../hook/useFetch'
+
 
 const JobDetails = () => {
+
+    const params = useSearchParams()
+    const router = useRouter()
+
+    const { data, isLoading, error, refresh } = useFetch("job-details", {
+        job_id: params.id
+    })
+
+
   return (
-    <div>
-      JobDetails
-    </div>
+    <Text>JobDetails</Text>
   )
 }
 
