f91892891d552228ae21ddb1160832a62afd8ab0
davidojo1144 <davco1144@gmail.com>
2025-06-21 04:46:23 +0100

    Mobile Application built for jobs using React Native

--- React_native_jobs/components/common/cards/nearby/NearbyJobCard.jsx
+++ React_native_jobs/components/common/cards/nearby/NearbyJobCard.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 import styles from './nearbyjobcard.style'
+import { cheeckImageURL } from '../../../../utils'
 
-const NearbyJobCard = () => {
+
+
+const NearbyJobCard = ({job, handleNavigate}) => {
   return (
-    <View>
-      <Text>NearbyJobCard</Text>
-    </View>
+    <TouchableOpacity
+    style={styles.container}
+    onPress={()=> handleNavigate}
+    >
+
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+        <Image
+        source={{uri: cheeckImageURL(item.employer_logo)
+          ? item.employer_logo
+          : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NNICW7diNmGXJfMicpY9eXHKV4sgzO5H.jpg'
+        }}
+        resizeMode='contain'
+        style={styles.logoImage}        />
+      </TouchableOpacity>
+      <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text>
+
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)}
+        numberOfLines={1}
+        >
+          {item.job_title}
+        </Text>
+        <Text styles={styles.location}>{item.job_country}</Text>
+      </View>
+    </TouchableOpacity>
   )
 }
 
-export default NearbyJobCard
+export default NearbyJobCardJobCard
--- React_native_jobs/components/common/cards/popular/PopularJobCard.jsx
+++ React_native_jobs/components/common/cards/popular/PopularJobCard.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 import styles from './popularjobcard.style'
+import { cheeckImageURL } from '../../../../utils'
+
+
 
 const PopularJobCard = ({item, selectedJob, handleCardPress}) => {
   return (
@@ -12,7 +15,10 @@ const PopularJobCard = ({item, selectedJob, handleCardPress}) => {
 
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
         <Image
-        source={{uri: item.employer_logo}}
+        source={{uri: cheeckImageURL(item.employer_logo)
+          ? item.employer_logo
+          : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NNICW7diNmGXJfMicpY9eXHKV4sgzO5H.jpg'
+        }}
         resizeMode='contain'
         style={styles.logoImage}        />
       </TouchableOpacity>
