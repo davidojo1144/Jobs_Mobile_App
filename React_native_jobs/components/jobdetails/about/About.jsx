25c7a0c512f5049152527b92b5585456918c7f54
davidojo1144 <davco1144@gmail.com>
2025-06-22 07:08:27 +0100

    Mobile Application built for jobs using React Native

--- React_native_jobs/components/jobdetails/about/About.jsx
+++ React_native_jobs/components/jobdetails/about/About.jsx
@@ -3,10 +3,14 @@ import { View, Text } from 'react-native'
 
 import styles from './about.style'
 
-const About = () => {
+const About = ({info}) => {
   return (
-    <View>
-      <Text>About</Text>
+    <View style={styles.container}>
+      <Text style={styles.headText}>About this Job: </Text>
+
+      <View>
+        <Text>{info}</Text>
+      </View>
     </View>
   )
 }
